6ae0c650b43333fa38c2e2d12ca534f350c82423
Sajal0208 <sajaldewangan@gmail.com>
2023-11-11 02:51:49 +0530

    First Commit

---
+++ src/LandingPage/LandingPage.tsx
@@ -0,0 +1,15 @@
+import { Box, Heading, Text } from '@chakra-ui/react'
+
+const LandingPage = () => {
+    return (
+        <Box className="bg-black w-screen h-screen text-white">
+            <Box className='flex flex-col justify-center items-center'> 
+                <Heading className="">
+                    Whatsapp Chatbot
+                </Heading>
+            </Box>
+        </Box>
+    )
+}
+
+export default LandingPage
